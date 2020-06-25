1029d790a515121d20579121d556610750b27eb9
Fran Budiman <franbudiman@Frans-MacBook-Air.local>
2020-06-25 15:49:37 -0700

    set up all basic functionalities

---
+++ src/Slideshow.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import images from './images.json';
+import './Slideshow.css';
+
+const initialState = {
+  images: [],
+  loading: true,
+  currentIdx: 0,
+  current: {}
+}
+
+class Slideshow extends Component {
+
+  state = {...initialState};
+
+  componentDidMount() {
+    this.setState(() => ({
+      images: images.files
+    }), this.setCurrentImg);
+  };
+
+  setCurrentIdx = (forward) => {
+    this.setState((prevState) => ({
+      currentIdx: forward ? 
+        prevState.currentIdx === images.files.length - 1 ?
+          0 : prevState.currentIdx + 1 : prevState.currentIdx === 0 ?
+            images.files.length - 1 : prevState.currentIdx - 1
+    }), this.setCurrentImg)
+  };
+
+  setCurrentImg = () => {
+    const { images: imgs, currentIdx } = this.state;
+    const current = {
+      ...imgs[currentIdx],
+      views: imgs[currentIdx].views + 1
+    };
+    const idx = imgs.findIndex((img) => img.id === current.id);
+    imgs.splice(idx, 1, current);
+
+    this.setState(() => ({ 
+      images: [...imgs],
+      current,
+      loading: false
+    }));
+  }
+
+  render() {
+    const { current, loading, images: imgs } = this.state;
+
+    return (
+      <div className="Slideshow">
+        <div className="Slideshow__left" onClick={() => this.setCurrentIdx(false)}></div>
+        <div className="Slideshow__image">
+          {loading ?
+            <div className="empty-image"></div> :
+            <img src={require(`./images/${current.url}`)} alt={current.title} />
+          }
+        </div>
+        <div className="Slideshow__left" onClick={() => this.setCurrentIdx(true)}></div>
+      </div>
+    );
+  }
+}
+
+export default Slideshow;
